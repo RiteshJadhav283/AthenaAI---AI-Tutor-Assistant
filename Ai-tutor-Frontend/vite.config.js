bc9a7f0ed178456b17b154970b926cc5dbdc0738
Ritesh Jadhav <ritesh6798@hotmail.com>
2025-07-25 09:40:25 +0530

    Trying to deploy 1

--- Ai-tutor-Frontend/vite.config.js
+++ Ai-tutor-Frontend/vite.config.js
@@ -5,10 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: 'AthenaAI---AI-Tutor-Assistant', // 👈 Add this line here
+
   server: {
     host: "::",
     port: 8080,
-    historyApiFallback: true, // <-- Add this line for SPA fallback
+    historyApiFallback: true,
   },
   plugins: [
     react(),
