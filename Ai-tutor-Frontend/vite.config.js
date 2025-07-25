9633bff174621f76652d94d13917c448e915315c
Ritesh Jadhav <ritesh6798@hotmail.com>
2025-07-25 09:43:37 +0530

    Removed i line in vite.config for vercel deployement

--- Ai-tutor-Frontend/vite.config.js
+++ Ai-tutor-Frontend/vite.config.js
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/AthenaAI---AI-Tutor-Assistant/', // 👈 Exactly this
 
   server: {
     host: "::",
